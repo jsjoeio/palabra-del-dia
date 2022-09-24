16d1fdc26f099c6cb32e7ec0352a76dd45a40c64
Joe Previte <jjprevite@gmail.com>
2022-09-24 17:38:03 +0000

    feat: more pwa stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <><Head>
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta
+      name='viewport'
+      content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+    />
+  </Head><Component {...pageProps} /></>
 }
 
 export default MyApp
